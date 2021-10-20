3fe3a6e0bb72cce369a3c32125023d11efab0c94
thatblindgeye <thatblindgeye@gmail.com>
2021-10-20 18:09:39 -0400

    Add additional ship tests

--- src/scripts/ship/shipFactory.js
+++ src/scripts/ship/shipFactory.js
@@ -1,4 +1,8 @@
 export default function shipFactory(shipLength) {
+  if (!shipLength || shipLength < 0 || typeof shipLength !== 'number') {
+    throw new Error('Ship length must be a number greater than 0');
+  }
+
   const createHitsArray = () => {
     const hitsArray = [];
     for (let i = 0; i < shipLength; i++) {
@@ -12,11 +16,19 @@ export default function shipFactory(shipLength) {
   const getLength = () => hits.length;
 
   const markHit = (position) => {
-    const hitsCopy = [...hits];
-    if (!hitsCopy[position]) {
-      hitsCopy[position] = true;
-      hits = [...hitsCopy];
+    if (position < 0 || typeof position !== 'number') {
+      throw new Error('Position must be a number greater than or equal to 0');
     }
+
+    const hitsCopy = hits.map((hit, index) => {
+      let newHit = hit;
+      if (position === index && !newHit) {
+        newHit = true;
+      }
+      return newHit;
+    });
+
+    hits = [...hitsCopy];
   };
 
   const isSunk = () => {
--- src/scripts/ship/shipFactory.test.js
+++ src/scripts/ship/shipFactory.test.js
@@ -1,8 +1,56 @@
 import shipFactory from './shipFactory';
 
 describe('Ship Factory', function () {
+  let testShip;
+
+  beforeEach(() => {
+    testShip = shipFactory(2);
+  });
+
   test('creates a ship', function () {
-    const testShip = shipFactory(6);
-    expect(testShip.getLength()).toBe(6);
+    expect(testShip.getLength()).toBe(2);
+  });
+
+  test('creates a ship that is not sunk', function () {
+    expect(testShip.isSunk()).toBe(false);
+  });
+
+  test('returns not sunk if all positions are not hit', function () {
+    testShip.markHit(1);
+    expect(testShip.isSunk()).toBe(false);
+  });
+
+  test('returns sunk if all positions are hit', function () {
+    testShip.markHit(0);
+    testShip.markHit(1);
+    expect(testShip.isSunk()).toBe(true);
+  });
+});
+
+describe('Ship Factory - Thrown Errors', function () {
+  let testShip;
+
+  test('throws error if length is not provided', function () {
+    expect(() => (testShip = shipFactory())).toThrow(
+      'Ship length must be a number greater than 0'
+    );
+  });
+
+  test('throws error if length is 0', function () {
+    expect(() => (testShip = shipFactory(0))).toThrow(
+      'Ship length must be a number greater than 0'
+    );
+  });
+
+  test('throws error if length is a negative number', function () {
+    expect(() => (testShip = shipFactory(-1))).toThrow(
+      'Ship length must be a number greater than 0'
+    );
+  });
+
+  test('throws error if length is not a number', function () {
+    expect(() => (testShip = shipFactory('0'))).toThrow(
+      'Ship length must be a number greater than 0'
+    );
   });
 });
